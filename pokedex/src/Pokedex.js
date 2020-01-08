663ea56d54e5d1d1286c25b01dfbb6640ecc77ab
Janne Nissinen <janne.nissinen@outlook.com>
2020-01-08 11:39:06 +0200

    Whitespaces

--- pokedex/src/Pokedex.js
+++ pokedex/src/Pokedex.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 import Pokecard from './Pokecard';
 import './Pokedex.css';
 
-
 class Pokedex extends Component {
-
-
-
 	render() {
 		var pokecards = this.props.pokemons.map((p) => {
 			return <Pokecard {...p} key={p.id} />
@@ -18,9 +14,7 @@ class Pokedex extends Component {
 				<h3>Total Experience: {this.props.exp}</h3>
 				<h3 className={this.props.isWinner ? 'Pokedex-winning' : 'Pokedex-losing'}>{this.props.isWinner ? 'Winning hand' : ' Losing Hand'}</h3>
 
-
 				<div className="Pokedex-pokemons">
-
 						{pokecards}
 				</div>
 			</div>
